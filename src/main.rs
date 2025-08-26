19931e66301b2f68ddd5069fad2ac11a28596ea1
so-lovely <lovelymoomal001@protonmail.com>
2025-08-26 21:43:27 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rain_quantum_simulator::{QuantumCircuit, QuantumGate, QuantumRegister};
 
-fn parse(program: &str) -> QuantumCircuit <f64> {
+fn parse(program: &str) -> QuantumCircuit<f64> {
     let mut circuit = QuantumCircuit::new();
     for line in program.lines() {
         let parts: Vec<&str> = 
@@ -8,7 +8,18 @@ fn parse(program: &str) -> QuantumCircuit <f64> {
         if parts.is_empty() {
             continue;
         }
-        println!("Status: {:?}", parts);
+        
+        let gate_name = parts[0];
+        let targets: Vec<usize> = parts[1..].iter().map(|s|s.parse().unwrap()).collect();
+
+        match gate_name.to_uppercase().as_str() {
+            "H" => circuit.add_gate(&targets, QuantumGate::h()),
+            "X" => circuit.add_gate(&targets, QuantumGate::x()),
+            "CNOT" => circuit.add_gate(&targets, QuantumGate::cnot()),
+            _ => {
+                println!("Warning: unknown gate:{}",gate_name);
+            }
+        }
     }
 
     circuit
@@ -18,5 +29,5 @@ fn main() {
     let quil_program = "H 0\nCNOT 0 1";
     println!("input:\n{}\n", quil_program);
     let circuit = parse(quil_program);
-    println!("parsed!");
+    println!("parsed! len {}", circuit.len());
 }
